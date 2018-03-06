91deeec4fd9bcffddc96656ccbf6a7524d1dc5f6
Artem Orobets <aorobets@smartling.com>
2018-03-05 22:52:51 -0500

    Simple main window with text area.
    Set up spring as a controller factory for JavaFX

--- src/main/java/io/github/enishlabs/logpad/LogpadApp.java
+++ src/main/java/io/github/enishlabs/logpad/LogpadApp.java
@@ -1,21 +1,45 @@
 package io.github.enishlabs.logpad;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class LogpadApp extends Application
 {
+    private AnnotationConfigApplicationContext context;
+    private Parent root;
+
+    @Override
+    public void init() throws Exception
+    {
+        context = new AnnotationConfigApplicationContext(LogpadConfiguration.class);
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main_window.fxml"));
+        fxmlLoader.setControllerFactory(context::getBean);
+        root = fxmlLoader.load();
+    }
+
     @Override
     public void start(Stage primaryStage)
     {
-        System.out.println("Crunch trhough logs!");
+        primaryStage.setTitle("LogPad");
+
+        primaryStage.setScene(new Scene(root));
 
         primaryStage.show();
     }
 
+    @Override
+    public void stop() throws Exception
+    {
+        context.close();
+    }
+
     public static void main(String[] args)
     {
-        launch(args);
+        launch(LogpadApp.class, args);
     }
 }
 
---
+++ src/main/java/io/github/enishlabs/logpad/LogpadConfiguration.java
@@ -0,0 +1,9 @@
+package io.github.enishlabs.logpad;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LogpadConfiguration
+{
+}
+
