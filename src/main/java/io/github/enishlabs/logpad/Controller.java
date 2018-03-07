de39b95af185a296c75bc6e1f6b17c64206ee611
Artem Orobets <aorobets@smartling.com>
2018-03-06 22:01:25 -0500

    Implemented open file functionality

---
+++ src/main/java/io/github/enishlabs/logpad/Controller.java
@@ -0,0 +1,40 @@
+package io.github.enishlabs.logpad;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+
+
+@Component
+public class Controller
+{
+    @FXML
+    private TextArea textArea;
+
+    public void close()
+    {
+        System.exit(0);
+    }
+
+    public void open() throws IOException
+    {
+        FileChooser fileChooser = new FileChooser();
+
+        File file = fileChooser.showOpenDialog(null);
+
+        textArea.clear();
+
+        List<String> readAllLines = Files.readAllLines(file.toPath());
+        for (int i = 0; i < readAllLines.size(); i++)
+        {
+            String l = readAllLines.get(i);
+            textArea.appendText(l + "\n");
+        }
+    }
+}
